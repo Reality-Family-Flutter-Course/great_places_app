5cf522e6c7efd6e4b273d788eb567cd828b4bd5f
Леонид <leonis13579@gmail.com>
2022-03-05 10:05:48 +0300

    1.3

--- lib/widgets/image_input.dart
+++ lib/widgets/image_input.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ImageInput extends StatefulWidget {
   ImageInput({Key? key}) : super(key: key);
@@ -11,6 +12,13 @@ class ImageInput extends StatefulWidget {
 
 class _ImageInputState extends State<ImageInput> {
   File? _storedImage;
+
+  _takePicture() async {
+    final imageFiale = await ImagePicker().pickImage(
+      source: ImageSource.camera,
+      maxWidth: 600,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -42,7 +50,7 @@ class _ImageInputState extends State<ImageInput> {
         ),
         Expanded(
           child: TextButton.icon(
-            onPressed: () {},
+            onPressed: _takePicture,
             icon: const Icon(Icons.camera),
             label: const Text("Камера"),
             style: TextButton.styleFrom(
