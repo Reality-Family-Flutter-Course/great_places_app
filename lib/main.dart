bf80fc73c6f947a0150133f34a92bfaa61a79d8d
Леонид <leonis13579@gmail.com>
2022-03-16 08:38:25 +0300

    1.15

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:great_places_app/models/place.dart';
 import 'package:great_places_app/providers/great_places.dart';
 import 'package:great_places_app/screens/add_place_screen.dart';
 import 'package:great_places_app/screens/places_list_screen.dart';
-import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
 
 void main() async {
-  final appDocumentDir = await path_provider.getApplicationDocumentsDirectory();
-  Hive.init(appDocumentDir.path);
+  await Hive.initFlutter();
+  Hive.registerAdapter(PlaceAdapter());
+  Hive.registerAdapter(PlaceLocationAdapter());
   runApp(const MyApp());
 }
 
